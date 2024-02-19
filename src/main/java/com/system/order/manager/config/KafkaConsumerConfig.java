1144a235a63c44a20f61b58ccb66dfcb85e4f0a4
Bruno Nascimento <brunoenig@hotmail.com>
2024-02-19 17:09:25 +0000

    Version 1.1.7

--- src/main/java/com/system/order/manager/config/KafkaConsumerConfig.java
+++ src/main/java/com/system/order/manager/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import com.system.order.manager.model.OrderRequest;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@ConditionalOnProperty(prefix = "kafka", name = "default", havingValue = "false")
 public class KafkaConsumerConfig {
 
     @Value(value = "${spring.kafka.bootstrap-servers}")
